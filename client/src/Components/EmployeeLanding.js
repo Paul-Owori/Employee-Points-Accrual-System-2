25ba095bdd59509b8ed5d5036616bd87db7a942b
“Paul <prowori2@gmail.com>
2019-06-11 10:11:26 +0300

    Add place preorder functionality

--- client/src/Components/EmployeeLanding.js
+++ client/src/Components/EmployeeLanding.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
-import { Container, Row, Col, Button } from "reactstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Form,
+  FormGroup,
+  Input,
+  Modal,
+  ModalHeader
+} from "reactstrap";
 import { pointsLeft } from "../PointCalculator/index";
 
 import { connect } from "react-redux";
@@ -13,10 +23,19 @@ import PropTypes from "prop-types";
 
 class EmployeeLanding extends Component {
   componentWillMount() {
+    console.log("this.props");
     const currentEmployee = JSON.parse(sessionStorage.getItem("employee"));
     currentEmployee
       ? this.setState({ employee: currentEmployee })
       : this.setState({ employee: {} });
+  }
+
+  componentWillReceiveProps() {
+    if (this.props.order.orders && this.props.order.orders.length) {
+      this.setState({
+        orders: this.props.order.orders
+      });
+    }
   }
 
   //   componentDidMount() {
@@ -31,10 +50,18 @@ class EmployeeLanding extends Component {
   state = {
     employee: {},
     loading: false,
-    orders: []
+    orders: [],
+    order_for: "",
+    order_price: "",
+    warnModal: false,
+    warnModalText: ""
   };
 
-  checker = () => {
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  };
+
+  orderChecker = () => {
     if (this.props.order.orders && this.props.order.orders.length) {
       return true;
     } else {
@@ -42,37 +69,131 @@ class EmployeeLanding extends Component {
     }
   };
 
+  preOrder = e => {
+    e.preventDefault();
+
+    if (
+      pointsLeft(this.state.employee) !== 0 &&
+      pointsLeft(this.state.employee) >= this.state.order_price
+    ) {
+      let order = {};
+
+      order.employee_id = this.state.employee.employee_id;
+      order.order_for = this.state.order_for;
+      order.order_price = this.state.order_price;
+      if (order.order_for && order.order_price) {
+        this.props.preOrder(order);
+
+        this.setState({
+          order_for: "",
+          order_price: "",
+          warnModalText:
+            "Your pre order has been placed successfuly to confirm it, please check your cart"
+        });
+        setTimeout(() => {
+          this.warnModaltoggle();
+        }, 500);
+      } else {
+        this.setState({
+          warnModalText:
+            "Something went wrong! Please check to make sure all fields have content."
+        });
+        setTimeout(() => {
+          this.warnModaltoggle();
+        }, 500);
+      }
+    } else {
+      this.setState({
+        warnModalText: "Sorry, You do not have enough points for this benefit"
+      });
+      setTimeout(() => {
+        this.warnModaltoggle();
+      }, 500);
+    }
+  };
+
+  confirmOrder = preOrders => {
+    this.props.addOrders(preOrders);
+  };
+
+  warnModaltoggle = () => {
+    this.setState({ warnModal: !this.state.warnModal });
+    setTimeout(() => {
+      this.setState({ warnModal: !this.state.warnModal });
+    }, 1500);
+  };
+
   render() {
     return (
       <Container>
-        <h2 className="text-center">
-          Welcome{" "}
-          {this.state.employee
-            ? this.state.employee.employee_firstName
-            : this.props.employee.employee
-            ? this.props.employee.employee.employee_firstName
-            : ""}
-          !
-        </h2>
-        <h2 className="text-center">
-          Your seniority is of tier{" "}
-          {this.state.employee
-            ? this.state.employee.employee_seniority
-            : this.props.employee.employee
-            ? this.props.employee.employee.seniority
-            : ""}
-          !
-        </h2>
-        <h2 className="text-center">
-          Your available points are{" "}
-          {this.state.employee ? pointsLeft(this.state.employee) : 0}!
-        </h2>
+        <div className="text-center">
+          <h2 className="text-center">
+            Welcome{" "}
+            {this.state.employee
+              ? this.state.employee.employee_firstName
+              : this.props.employee.employee
+              ? this.props.employee.employee.employee_firstName
+              : ""}
+            !
+          </h2>
+          <h5 className="text-center">
+            Your seniority is of tier{" "}
+            {this.state.employee
+              ? this.state.employee.employee_seniority
+              : this.props.employee.employee
+              ? this.props.employee.employee.seniority
+              : ""}
+            !
+          </h5>
+          <h5 className="text-center">
+            Your available points are{" "}
+            {this.state.employee ? pointsLeft(this.state.employee) : 0}!
+          </h5>
 
-        <p>The following buttons are only for testing purposes;</p>
+          <p>Place an order;</p>
+        </div>
 
-        <Container className="mb-5">
-          <Row />
-        </Container>
+        <Row className="text-center">
+          <Col xs="6" className="text-center">
+            <Form className="text-center border" onSubmit={this.preOrder}>
+              <FormGroup>
+                <Input
+                  type="text"
+                  className="form-control"
+                  id="order_for"
+                  name="order_for"
+                  placeholder="What would you like to order for?"
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+              <FormGroup className="mb-5 ">
+                <Input
+                  type="Number"
+                  className="form-control"
+                  id="order_price"
+                  name="order_price"
+                  placeholder="How many points does the order cost? *Numbers only*"
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+
+              <Button
+                color="light"
+                block
+                type="submit"
+                className="mb-3 align-bottom"
+              >
+                Place pre Order
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+
+        <Modal isOpen={this.state.warnModal} toggle={this.warnModaltoggle}>
+          <ModalHeader toggle={this.warnModaltoggle}>
+            {this.state.warnModalText}
+          </ModalHeader>
+        </Modal>
       </Container>
     );
   }
